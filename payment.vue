6a0250e6927587884915624b158c2feab537daba
prachi-3123 <74055230+prachi-3123@users.noreply.github.com>
2023-01-21 16:19:47 +0530

    View

---
+++ payment.vue
@@ -0,0 +1,135 @@
+<template>
+<div class="min-w-screen min-h-screen bg-gray-200 flex items-center justify-center px-5 pb-10 pt-16" style="background-color: lightblue;width: 825px;height: 465px;">
+    <div class="w-full mx-auto rounded-lg bg-white shadow-lg p-5 text-gray-700" style="max-width: 600px">
+        <div class="w-full pt-1 pb-5">
+            <div class="bg-indigo-500 text-white overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg flex justify-center items-center">
+                <i class="mdi mdi-credit-card-outline text-3xl"></i>
+            </div>
+        </div>
+        <div class="mb-10">
+            <h1 class="text-center font-bold text-xl uppercase">Secure payment scan</h1>
+        </div>
+        <div class="mb-3 flex -mx-2">
+            <div class="px-2" style="width: 180px;">
+                <label for="type1" class="flex items-center cursor-pointer">
+                    <input type="radio" class="form-radio h-5 w-5 text-indigo-500" name="type" id="type1" checked>
+                    
+                </label>
+            </div>
+            <div class="px-2">
+                <label for="type2" class="flex items-center cursor-pointer">
+                    <input type="radio" class="form-radio h-5 w-5 text-indigo-500" name="type" id="type2">
+                    
+                </label>
+            </div>
+        </div>
+        <div class="mb-3">
+            <label class="font-bold text-sm mb-2 ml-1"></label>
+            <div>
+                <input class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="Prachi Soni" type="text"/>
+            </div>
+        </div>
+       <div>Date:</div>
+        <div class="mb-3 -mx-2 flex items-end">
+            <div class="px-2 w-1/2" style="width: 180px;">
+                <label class="font-bold text-sm mb-2 ml-1"></label>
+
+
+
+                <div>
+                    <select class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                        <option value="01">01 </option>
+                        <option value="02">02 </option>
+                        <option value="03">03</option>
+                        <option value="04">04</option>
+                        <option value="05">05</option>
+                        <option value="06">06</option>
+                        <option value="07">07</option>
+                        <option value="08">08 </option>
+                        <option value="09">09 </option>
+                        <option value="10">10 </option>
+                        <option value="11">11 </option>
+                        <option value="12">12 </option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                        <option value="24">24</option>
+                        <option value="25">25</option>
+                        <option value="26">26</option>
+                        <option value="27">27</option>
+                        <option value="28">28</option>
+                        <option value="29">29</option>
+                        <option value="30">30</option>
+                        <option value="31">31</option>
+                        
+                    </select>
+
+
+
+
+                <div>
+                    <select class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                        <option value="01">01 - January</option>
+                        <option value="02">02 - February</option>
+                        <option value="03">03 - March</option>
+                        <option value="04">04 - April</option>
+                        <option value="05">05 - May</option>
+                        <option value="06">06 - June</option>
+                        <option value="07">07 - July</option>
+                        <option value="08">08 - August</option>
+                        <option value="09">09 - September</option>
+                        <option value="10">10 - October</option>
+                        <option value="11">11 - November</option>
+                        <option value="12">12 - December</option>
+                    </select>
+                </div>
+            </div>
+            <div class="px-2 w-1/2">
+                <select class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
+                    <option value="2022">2022</option>
+                    <option value="2023">2023</option>
+                    <option value="2024">2024</option>
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+                    <option value="2027">2027</option>
+                    <option value="2028">2028</option>
+                    <option value="2029">2029</option>
+                </select>
+            </div>
+        </div>
+
+
+        <div style="width: 100px;"> 
+                <img height="20px" src="src\assets\qrcode.jpeg" >
+
+
+
+        </div>
+
+
+        <div class="mb-10">
+            <label class="font-bold text-sm mb-2 ml-1">transcation id</label>
+            <div>
+                <input class="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="Trascation id" type="text"/>
+            </div>
+        </div>
+        <div>
+            <button class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
+                <i class="mdi mdi-lock-outline mr-1"></i> 
+                <a href="/thanku" type="button">
+                PAY NOW</a></button>
+        </div>
+    </div>
+</div>
+</div>
+</template>
---
+++ xoxo.vue
@@ -0,0 +1,187 @@
+<template>
+  <div style="width: 700px;height: 400px;">
+    <header class=x>
+
+        <button style="padding-right: 0px;right: auto;left: 400px;" v-on:click="navigateTo('products')">View Product</button>
+
+     <div style="right: 150px; color:black"> {{cart.length}} in cart</div>
+    
+      <button style="right: auto;left: 400px;" v-on:click="navigateTo('cart')">view cart</button>
+
+</header>
+
+
+
+
+<div v-if="page ==='cart'">
+ 
+ 
+
+  <div>total: {{total}} </div>
+    <h1 style="font-size:50px">Your Cart</h1>
+    <div class="products">
+      <div v-for="(product, index) in cart" :key="index">
+        <h3>{{product.name}}</h3>
+        <div class="cost">{{product.cost}}</div>
+        <div class="context">{{product.context}}</div>
+        <img :src="product.image" />
+        <button v-on:click="removedItemFromCart(product)">Removed from cart</button>
+      </div>
+
+    </div>
+ 
+ <div> <button style="color: green"><a href="/payment" type="button"> checkout</a></button></div>
+</div>
+
+<div v-if="page ==='products'">
+     <h1 style="font-size:50px;">BANGLES</h1>
+    <div class="products">
+     <div v-for="(product, index) in products" :key="index">
+        <h3>{{product.name}}</h3>
+        <div class="cost">{{product.cost}}</div>
+        <div class="context">{{product.context}}</div>
+        <img :src="product.image" />
+        <button v-on:click="addItemToCart(product)">Add to cart</button>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      page:"products",
+      cart:[],
+      products: [
+        {
+          name: "Design_No: 007846",
+          cost: "Cost: ₹8000",
+          context:"Details: Classic with 18 cart of gold",
+          image:
+            "https://th.bing.com/th/id/OIP.Tt-raGkFWdb7GLW0YAv-pgHaHB?pid=ImgDet&rs=1"
+        },
+        
+        {
+          name: "Design_No: 009985",
+          cost: "Cost: ₹12000",
+          context:"Details: classic with 18 cart of gold",
+          image:
+            "https://5.imimg.com/data5/GE/AH/PY/SELLER-84633020/designer-gold-bangles--250x250.jpg"
+        },
+        {name: "Design_No: 008765",
+          cost: "Cost: ₹19000",
+          context:"Details: classic with 18 cart of gold",
+          image:
+            "https://savoryjewellery.com/wp-content/uploads/2018/05/SJBA068-Traditional-Gold-Bangles.jpg"
+        },
+        {name: "Design_No: 005433",
+          cost: "Cost: ₹22000",
+          context:"Details: classic with 18 cart of gold",
+          image:
+            "https://www.dhanalakshmijewellers.com/wp-content/uploads/2020/03/HollowGoldBangles3.jpg"
+        },
+        {name: "Design_No: 006722",
+          cost: "Cost: ₹17000",
+          context:"Details: classic with 18 cart of gold",
+          image:
+            "https://image.ejohri.com/catalog/product/145/4CHU15-1.jpg"
+        },
+        {name: "Design_No: 002267",
+          cost: "Cost: ₹12000",
+          context:"Details: classic with 18 cart of gold",
+          image:
+            "http://3.bp.blogspot.com/-0V9jYT6cCGY/UOf5H_Lpq7I/AAAAAAAAHuc/MYd5p33VKvM/s1600/Gold-Bangle-Collection.jpg"
+        },
+        {name: "Design_No: 008742",
+          cost: "Cost: ₹21000",
+          context:"Details: classic with 18 cart of gold",
+          image:
+            "https://kinclimg1.bluestone.com/f_jpg,c_scale,w_515,b_rgb:f0f0f0/giproduct/BIDG0086B41_YAA18DIG6XXXXXXXX_ABCD00-PICS-00002-1024-33626.png"
+        },
+        {name: "Design_No: 006731",
+          cost: "Cost: ₹20100",
+          context:"Details: classic with 18 cart of gold",
+          image:
+            "https://5.imimg.com/data5/PS/YF/UL/SELLER-27282183/gold-bangles-500x500.jpg"
+        },
+        
+      ]
+    };
+  },
+  methods: {
+    addItemToCart(product) {
+      this.cart.push(product);
+    },
+    removedItemFromCart(product){
+      this.cart.splice(this.cart.indexOf(product));
+    },
+    navigateTo(page){
+      this.page=page;
+    },
+    cartTotal(state,getters){
+      let total=0
+      getters.product.forEach(product => {
+        total+= product.cost
+        
+      });
+      return total
+    }
+  },
+  components: {}
+};
+</script>
+
+
+
+
+<style >
+  body{
+    margin:0;
+    overflow: hidden;
+  }
+
+</style>
+
+
+<style scoped>
+.products{
+  display:grid;
+  width: 130%;
+  grid-template-columns:1fr 1fr 1fr ;
+  grid-gap: 40px;
+  color:#212121;
+  background-color: peachpuff;
+}
+
+.x{
+  height: 100px;
+  width: 130%;
+  background-color:peachpuff;
+  box-shadow: 2px 2px 5px #999;
+  text-align: center;
+  font-size: 25px;
+  height: 100px;
+  padding-top: 20px;
+}
+
+button{
+    width: 200px;
+    padding: 10px 0;
+    text-align: center;
+    margin: 10px 05px;
+    border-radius: 20px;
+    font-weight: bold;
+    border: 2px solid #009688;
+    background: transparent;
+    color: #212121;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+}
+
+
+</style>
